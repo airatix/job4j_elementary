c9634018628fa46d886a238391e364ddfc63b7a1
airatix <airatix@mail.ru>
2022-08-01 10:19:14 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        if (name.codePointAt(0) > 122 ||  name.codePointAt(0) < 97) {
+            return false;
+        }
+        for (int i = 1; i < name.length(); i++) {
+            if (name.codePointAt(i) == 36 || name.codePointAt(i) == 95) {
+                continue;
+            } else if (name.codePointAt(i) >= 65 && name.codePointAt(i) <= 90) {
+                continue;
+            } else if (name.codePointAt(i) >= 48 &&  name.codePointAt(i) <= 57) {
+                continue;
+            } else if (name.codePointAt(i) > 122 &&  name.codePointAt(i) < 97) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
